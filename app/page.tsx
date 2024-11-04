3eed7a8e45665dc9b8a7432b79c065378d9ecd7f
krishnapriya-venkatakrishnan <krishnapriyavenkatakrishnan@gmail.com>
2024-11-04 12:55:30 +0100

    ESLint & Prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
-  return (
-    <h1 className="text-2xl font-light text-center text-purple-400">Welcome to the world of Next.js</h1>
-  );
-}
+    return (
+        <h1 className="text-center text-2xl font-light text-purple-400">
+            Welcome to the world of Next.js
+        </h1>
+    );
+};
 
 export default Home;
