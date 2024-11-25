6ab36d47238457d348248797330f7e3ef040a841
krishnapriya-venkatakrishnan <krishnapriyavenkatakrishnan@gmail.com>
2024-11-25 16:37:29 +0100

    Setup side bar mobile navigation and navigation links

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import MobileNavigation from "./MobileNavigation";
 import Theme from "./Theme";
 
 const Navbar = () => {
@@ -20,6 +21,7 @@ const Navbar = () => {
       <p>Global search bar</p>
       <div className="flex-between gap-5">
         <Theme />
+        <MobileNavigation />
       </div>
     </nav>
   );
