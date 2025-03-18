2288150b0f616207faca4e9e7a86ccacfd31e468
krishnapriya-venkatakrishnan <krishnapriyavenkatakrishnan@gmail.com>
2025-03-18 17:15:38 +0100

    Implement Pagination; Bug fix- Compare the _id property of the question?.author object with the session.user?.id, while showing the question details- edit page.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,6 +10,7 @@ import { EMPTY_QUESTION } from "@/constants/states";
 import { getQuestions } from "@/lib/actions/question.action";
 import CommonFilter from "@/components/filters/CommonFilter";
 import { HomePageFilters } from "@/constants/filters";
+import Pagination from "@/components/Pagination";
 
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
@@ -25,7 +26,7 @@ const Home = async ({ searchParams }: SearchParams) => {
     filter: filter || "",
   });
 
-  const { questions } = data || {};
+  const { questions, isNext } = data || {};
 
   return (
     <>
@@ -65,6 +66,7 @@ const Home = async ({ searchParams }: SearchParams) => {
           </div>
         )}
       />
+      <Pagination page={page} isNext={isNext || false} />
     </>
   );
 };
--- app/(root)/questions/[id]/edit/page.tsx
+++ app/(root)/questions/[id]/edit/page.tsx
@@ -17,7 +17,7 @@ const EditQuestion = async ({ params }: RouteParams) => {
   const { data: question, success } = await getQuestion({ questionId: id });
   if (!success) return notFound();
 
-  if (question?.author !== session.user?.id) redirect(ROUTES.QUESTION(id));
+  if (question?.author._id !== session.user?.id) redirect(ROUTES.QUESTION(id));
 
   return (
     <>
