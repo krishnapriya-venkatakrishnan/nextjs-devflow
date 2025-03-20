d554417c715abddf5156ea639d534f4c17547166
krishnapriya-venkatakrishnan <krishnapriyavenkatakrishnan@gmail.com>
2025-03-20 16:16:14 +0100

    Implement Stats UI

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -7,6 +7,7 @@ import { RouteParams } from "@/types/global";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import dayjs from "dayjs";
+import Stats from "@/components/user/Stats";
 
 const Profile = async ({ params }: RouteParams) => {
   const { id } = await params;
@@ -31,52 +32,67 @@ const Profile = async ({ params }: RouteParams) => {
     user;
 
   return (
-    <section className="flex flex-col-reverse items-start justify-between sm:flex-row">
-      <div className="flex flex-col items-start gap-4 lg:flex-row">
-        <UserAvatar
-          id={_id}
-          name={name}
-          imageUrl={image}
-          className="size-[140px] rounded-full object-cover"
-          fallbackClassName="text-6xl fond-bolder"
-        />
-        <div className="mt-3">
-          <h2 className="h2-bold text-dark100_light900">{name}</h2>
-          <p className="paragraph-regular text-dark200_light800">@{username}</p>
+    <>
+      <section className="flex flex-col-reverse items-start justify-between sm:flex-row">
+        <div className="flex flex-col items-start gap-4 lg:flex-row">
+          <UserAvatar
+            id={_id}
+            name={name}
+            imageUrl={image}
+            className="size-[140px] rounded-full object-cover"
+            fallbackClassName="text-6xl font-extrabold"
+          />
+          <div className="mt-3">
+            <h2 className="h2-bold text-dark100_light900">{name}</h2>
+            <p className="paragraph-regular text-dark200_light800">
+              @{username}
+            </p>
 
-          <div className="mt-5 flex-start flex-wrap gap-5">
-            {portfolio && (
+            <div className="mt-5 flex-start flex-wrap gap-5">
+              {portfolio && (
+                <ProfileLink
+                  imgUrl="/icons/link.svg"
+                  href={portfolio}
+                  title="Portfolio"
+                />
+              )}
+              {location && (
+                <ProfileLink imgUrl="/icons/location.svg" title="Location" />
+              )}
               <ProfileLink
-                imgUrl="/icons/link.svg"
-                href={portfolio}
-                title="Portfolio"
+                imgUrl="/icons/calendar.svg"
+                title={dayjs(createdAt).format("MMMM YYYY")}
               />
-            )}
-            {location && (
-              <ProfileLink imgUrl="/icons/location.svg" title="Location" />
-            )}
-            <ProfileLink
-              imgUrl="/icons/calendar.svg"
-              title={dayjs(createdAt).format("MMMM YYYY")}
-            />
-          </div>
+            </div>
 
-          {bio && (
-            <p className="paragraph-regulr text-dark400_light800 mt-8">{bio}</p>
+            {bio && (
+              <p className="paragraph-regulr text-dark400_light800 mt-8">
+                {bio}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
+          {loggedInUser?.user?.id === id && (
+            <Link href={`/profile/${id}/edit`}>
+              <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-12 min-w-44 px-4 py-3">
+                Edit Profile
+              </Button>
+            </Link>
           )}
         </div>
-      </div>
-
-      <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
-        {loggedInUser?.user?.id === id && (
-          <Link href={`/profile/${id}/edit`}>
-            <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-12 min-w-44 px-4 py-3">
-              Edit Profile
-            </Button>
-          </Link>
-        )}
-      </div>
-    </section>
+      </section>
+      <Stats
+        totalQuestions={totalQuestions}
+        totalAnswers={totalAnswers}
+        badges={{
+          GOLD: 0,
+          SILVER: 0,
+          BRONZE: 0,
+        }}
+      />
+    </>
   );
 };
 
