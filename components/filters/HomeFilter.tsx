d1d756076f03bcdcbb09a68fa8b064d112f568d1
krishnapriya-venkatakrishnan <krishnapriyavenkatakrishnan@gmail.com>
2025-04-18 11:03:32 +0200

    Implement the Recommendation System.

--- components/filters/HomeFilter.tsx
+++ components/filters/HomeFilter.tsx
@@ -7,13 +7,10 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { formUrlQuery, removeKeysFromUrlQuery } from "@/lib/url";
 
 const filters = [
-  { name: "React", value: "react" },
-  { name: "JavaScript", value: "javascript" },
-
-  // { name: "Newest", value: "newest" },
-  // { name: "Popular", value: "popular" },
-  // { name: "Unanswered", value: "unanswered" },
-  // { name: "Recommeded", value: "recommended" },
+  { name: "Newest", value: "newest" },
+  { name: "Popular", value: "popular" },
+  { name: "Unanswered", value: "unanswered" },
+  { name: "Recommeded", value: "recommended" },
 ];
 
 const HomeFilter = () => {
