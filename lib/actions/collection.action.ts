9ed9caf6a1315427e85ad75903b25e0c569814aa
krishnapriya-venkatakrishnan <krishnapriyavenkatakrishnan@gmail.com>
2025-04-20 12:37:47 +0200

    Fix- Handle empty collection page; Access Top posts button when in Answers section- Profile page.

--- lib/actions/collection.action.ts
+++ lib/actions/collection.action.ts
@@ -204,7 +204,7 @@ export async function getSavedQuestions(
 
     const questions = await Collection.aggregate(pipeline);
 
-    const isNext = totalCount.count > skip + questions.length;
+    const isNext = totalCount?.count > skip + questions.length;
 
     return {
       success: true,
